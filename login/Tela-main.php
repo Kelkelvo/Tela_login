8bd3903706411959191360fee83967b3688dc0cb
Kelkelvo <eukelwin@gmail.com>
2023-12-15 20:08:21 -0300

    introducting a css
    Comecei a estilizar o meu formulario, meu próximo passo é terminar de vez o css e substituir aqueles inputs de cidade, estado e logradouro por uma caixinha de digitar o cep e preencher automaticamente, mas como é sexta vou aproveitar um pouco do meu fim de semana

--- login/Tela-main.php
+++ login/Tela-main.php
@@ -14,31 +14,56 @@
             <legend>
                 <b> Faça seu Cadastro </b>
             </legend>
-            <br>
+            <br><br>
             <div class="Input">
                 <input type="text" name="nome" id="nome" class="inputUser" required>
                 <label for="nome"> Nome completo </label>
             </div>
-            <br>            
+            <br><br>            
             <div class="Input">
                 <input type="email" name="email" id="email" class="inputUser" required>
                 <label for="email"> Email </label>
             </div>
-            <br>
+            <br><br>
             <div class="Input">
                 <input type="tel" name="telefone" id="telefone" class="inputUser" required>
                 <label for="telefone"> Telefone </label>
             </div>
             
             <p> Gênero </p>
-            <input type="radio" id="feminino" name="genero" value="feminino" required>
-            <label for="feminino"> Feminino </label>
+                <input type="radio" id="feminino" name="genero" value="feminino" required>
+                <label for="feminino"> Feminino </label>
             <br>
-            <input type="radio" id="masculino" name="genero" value="masculino" required>
-            <label for="masculino"> Masculino </label>
+                <input type="radio" id="masculino" name="genero" value="masculino" required>
+                <label for="masculino"> Masculino </label>
             <br>
-            <input type="radio" id="outro" name="genero" value="outro" required>
-            <label for="outro"> Outro </label>
+                <input type="radio" id="outro" name="genero" value="outro" required>
+                <label for="outro"> Outro </label>
+            <br><br>
+            <div class="Input">
+                <label for="aniversario"> <b> Data de nascimento </b></label>
+                <input type="date" name="aniversario" id="aniversario" class="inputUser" required>
+            </div>
+            <br><br>
+            <div class="Input">
+                <input type="cidade" name="cidade" id="cidade" class="inputUser" required>
+                <label for="cidade"> Cidade </label>
+            </div>
+            <br><br>            
+            <div class="Input">
+                <input type="estado" name="estado" id="estado" class="inputUser" required>
+                <label for="estado"> Estado </label>
+            </div>
+            <br><br>
+            <div class="Input">
+                <input type="logradouro" name="logradouro" id="logradouro" class="inputUser" required>
+                <label for="logradouro"> Logradouro </label>
+            </div>
+            <br><br>
+            <div>
+                <input type="submit" name="submit" id="submit">
+            </div>
+
 
         </fieldset>
     </div>    
